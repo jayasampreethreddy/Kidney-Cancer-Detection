a2329a7531c4400d3d79964ac0b1591fdd9f8d36
Jaya Sampreeth Reddy <kondejayasampreethreddy@gmail.com>
2024-09-04 19:39:52 +0530

    commit 4

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path(r"C:\Users\konde\Music\projects\Kidney Cancer Detection\Kidney-Cancer-Detection\config\config.yaml")
+PARAMS_FILE_PATH = Path(r"C:\Users\konde\Music\projects\Kidney Cancer Detection\Kidney-Cancer-Detection\params.yaml")
